1fe5f35e8d7a35ec305baf62cd8551d7135a4bc9
Pavel Ivanoff <cloudever@mail.ru>
2018-10-29 00:32:37 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,82 @@
+
+# React-hooky
+
+## Why
+
+Since the presentation of hooks in React, many packages have been published for various purposes, and in order not to pull many new dependencies, most of them are combined in this package through re-export
+
+Package just proxy dependencies and does need to be compiled, it supports ESM for `webpack` and `rollup`.
+
+## Installation
+
+Using Yarn
+
+```
+yarn add react-hooky
+```
+
+or NPM
+
+```
+npm install react-hooky
+```
+
+## What's included
+
+### [the-platform](https://github.com/palmerhq/the-platform)
+
+ - `<Audio /> `
+ - `<Preload /> `
+ - `<Script /> `
+ - `<Stylesheet /> `
+ - `<Video /> `
+ - `useDeviceMotion`
+ - `useDeviceOrientation`
+ - `useGeoPosition`
+ - `useIntersectionObserver`
+ - `useMedia`
+ - `useNetworkStatus`
+ - `useWindowScrollPosition`
+ - `useWindowSize`
+
+### [react-use](https://github.com/streamich/react-use)
+
+- `useAudio`
+- `useBattery`
+- `useCounter`
+- `useCss`
+- `useFavicon`
+- `useGeolocation`
+- `useHover`
+- `useIdle`
+- `useLifecycles`
+- `useList`
+- `useLocalStorage`
+- `useLocation`
+- `useLogger`
+- `useMap`
+- `useMediaDevices`
+- `useMotion`
+- `useMount`
+- `useNetwork`
+- `useOrientation`
+- `useRaf`
+- `useSetState`
+- `useSize`
+- `useSpeech`
+- `useSpring`
+- `useTimeout`
+- `useTitle`
+- `useToggle`
+- `useTween`
+- `useUnmount`
+- `useWindowSize` as `useWindowSize2`
+- `useMedia` as `useMedia2`
+
+## Whan does `hooky` means?
+
+Just `-y` suffix is in Russian like `-s (-es)` is in English and is used for plural form
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,50 @@
+// the-platform
+export { Audio } from 'the-platform';
+export { Img } from 'the-platform';
+export { Preload } from 'the-platform';
+export { Script } from 'the-platform';
+export { Stylesheet } from 'the-platform';
+export { Video } from 'the-platform';
+export { useDeviceMotion } from 'the-platform';
+export { useDeviceOrientation } from 'the-platform';
+export { useGeoPosition } from 'the-platform';
+export { useIntersectionObserver } from 'the-platform';
+export { useMedia } from 'the-platform';
+export { useNetworkStatus } from 'the-platform';
+export { useWindowScrollPosition } from 'the-platform';
+export { useWindowSize } from 'the-platform';
+
+// react-use
+export { useAsync } from 'react-use';
+export { useAudio } from 'react-use';
+export { useBattery } from 'react-use';
+export { useCounter } from 'react-use';
+export { useCss } from 'react-use';
+export { useFavicon } from 'react-use';
+export { useGeolocation } from 'react-use';
+export { useHover } from 'react-use';
+export { useIdle } from 'react-use';
+export { useLifecycles } from 'react-use';
+export { useList } from 'react-use';
+export { useLocalStorage } from 'react-use';
+export { useLocation } from 'react-use';
+export { useLogger } from 'react-use';
+export { useMap } from 'react-use';
+export { useMediaDevices } from 'react-use';
+export { useMotion } from 'react-use';
+export { useMount } from 'react-use';
+export { useNetwork } from 'react-use';
+export { useOrientation } from 'react-use';
+export { useRaf } from 'react-use';
+export { useSetState } from 'react-use';
+export { useSize } from 'react-use';
+export { useSpeech } from 'react-use';
+export { useSpring } from 'react-use';
+export { useTimeout } from 'react-use';
+export { useTitle } from 'react-use';
+export { useToggle } from 'react-use';
+export { useTween } from 'react-use';
+export { useUnmount } from 'react-use';
+export { useWindowSize as useWindowSize2 } from 'react-use';
+export { useMedia as useMedia2 } from 'react-use';
+
