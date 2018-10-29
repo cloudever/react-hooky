548eb8e036fce1ae2fb64a9ec97195b3b1e32b8d
Pavel Ivanoff <cloudever@mail.ru>
2018-10-29 21:48:29 +0300

    test: rewrite test case title

--- index.test.js
+++ index.test.js
@@ -3,7 +3,7 @@ const pkg = require('./package.json');
 
 let bundle;
 
-it('contains all exports', async () => {
+it('supported by bundler', async () => {
   bundle = await rollup.rollup({
     input: pkg.module,
     external: Object.keys(pkg.peerDependencies),
